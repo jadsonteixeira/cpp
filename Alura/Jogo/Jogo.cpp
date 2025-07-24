c4a2c7507ff27b26337bb123d16fb3d9dd4aced4
Jadson Teixeira <jadsontxm@gmail.com>
2025-07-24 14:43:52 -0300

    Inicicando repositório

---
+++ Alura/Jogo/Jogo.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std; // falando que vou usar várias funções padrão do C++
+
+int main() {
+    system("cls");
+    
+    cout << "Adivinhe o numero: " << endl;
+
+    int numSecreto = 42;
+
+    cout << "Numero secreto: " << numSecreto << endl;
+
+    return 0;
+}
